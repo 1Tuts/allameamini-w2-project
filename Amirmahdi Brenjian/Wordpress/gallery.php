17482f87ff5953a07e0d256c70755a253f7dd363
Amirmahdi Brenjian <a.brenjian@1dws.com>
2012-06-23 12:45:18 +0430

    add lightbox!

---
+++ Amirmahdi Brenjian/Wordpress/gallery.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url') ?>/reset.css" />
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url') ?>/1styles.css" />
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url') ?>/style.css" />
+<link rel="stylesheet" type="text/css" href="lightbox.css" media="screen" <?php bloginfo('template_url') ?>/lightbox.css /> <!-- CSS for Lightbox -->
+
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/jquery.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/script.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/lightbox.js"></script>
+</head>
+<body>
+    <div class="gallery">
+    
+      <a href="images/gallery1/1.jpg" title="text 1 ...." rel="lightbox" >
+        <img src="images/gallery1/thumb1.jpg" alt="" />
+      </a>
+    
+      <a href="images/gallery1/2.jpg" title="text 2 ...." rel="lightbox" >
+        <img src="images/gallery1/thumb2.jpg" alt="" />
+      </a>
+    
+      <a href="images/gallery1/3.jpg" title="text 3 ...." rel="lightbox" >
+        <img src="images/gallery1/thumb3.jpg" alt="" />
+      </a>
+    
+    
+    </div>
+</body>
+</html>
